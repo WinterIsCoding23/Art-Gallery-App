885c09ec7c3b0bafa3d9c6b43128d30d2a057d69
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-04 00:06:39 +0200

    style button with color, background-color and box-shadow

--- components/FavoriteButton/index.js
+++ components/FavoriteButton/index.js
@@ -1,11 +1,18 @@
 //import {ReactComponent as Logo} from "../../assets"; 
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    background-color: red;
+    color: white;
+    box-shadow: 2px 2px pink;
+    `
 
 export default function FavoriteButton ( {isFavorite, slug, handleLike} ){
 
     return (
-        <button type="button" onClick={()=> {handleLike(slug)}} >
+        <StyledButton type="button" onClick={()=> {handleLike(slug)}} >
             {/* <Logo /> */}
-            {isFavorite ? "I don't like this anymore" : "Mark as favorite"}
-        </button>
+            {isFavorite ? "💔 I don't like this anymore" : "❤ Mark as favorite"}
+        </StyledButton>
     )
 }
